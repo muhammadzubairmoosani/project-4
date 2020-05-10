d033433c2c588306612b71af181429e1473a8682
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2020-05-11 01:17:08 +0500

    fix some issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Dashboard from "./Dashboard";
 import LeaderBoard from "./LeaderBoard";
 import Login from "./Login";
--- src/components/AuthenticatedRoutes.js
+++ src/components/AuthenticatedRoutes.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
